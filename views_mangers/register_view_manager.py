3272a6bb18b66e2059c857ecbfd6bc6142189801
yosef <yousefelmesalamy@gmail.com>
2023-01-07 20:25:47 +0200

    add icons make manager file for(regist & goal)

---
+++ views_mangers/register_view_manager.py
@@ -0,0 +1,62 @@
+from PyQt5 import QtWidgets , QtCore
+from views import register_view
+import json
+import requests
+import os
+class Register_Manager(QtWidgets.QWidget, register_view.Ui_Form):
+    loginAcceptedSignal = QtCore.pyqtSignal()
+    def __init__(self):
+        super(Register_Manager, self).__init__()
+        self.setupUi(self)
+
+        self.login_pushbutton.clicked.connect(self.handle_login)
+        # self.base_url = "https://saied.pythonanywhere.com/login/"
+        # self.userToken = ''
+
+
+    def handle_login(self):
+        # msg = QtWidgets.QMessageBox()
+        # username = self.username_lin.text()
+        # password = self.password_lin.text()
+        # if len(username) == 0:
+        #     msg.setWindowTitle("Warning")
+        #     msg.setText("you must fill all fields !")
+        #     msg.exec_()
+        # elif len(password) == 0:
+        #     msg.setWindowTitle("Warning")
+        #     msg.setText("you must fill all fields !")
+        #     msg.exec_()
+        # else:
+        #     data = {
+        #             "username": username,
+        #             "password": password
+        #             }
+        #     try:
+        #         self.user_check = requests.post(self.base_url, data=data).json()
+        #     except (requests.ConnectionError, requests.Timeout) as exception:
+        #         print(exception)
+        #         msg.setWindowTitle("Warning")
+        #         msg.setText("No internet connection.")
+        #         msg.exec_()
+        #     try :
+        #         for loginStatus in self.user_check.keys() :
+        #             if loginStatus == "non_field_errors" :
+        #                 msg.setWindowTitle("Warning")
+        #                 msg.setText("The user name or password was incorrect !")
+        #                 msg.exec_()
+        #             elif loginStatus == "token" :
+        #                 self.userToken = self.user_check[loginStatus]
+        #                 self.loginAcceptedSignal.emit()
+        #     except Exception as x:
+        #         print(x)
+        self.loginAcceptedSignal.emit()
+    def clear(self):
+        self.username_lin.setText("")
+        self.password_lin.setText("")
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    w = Register_Manager()
+    w.show()
+    # app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    app.exec_()
