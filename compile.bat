5fd93a68fb6478b51e3787d2c08c3aa96c806ff0
yosef <yousefelmesalamy@gmail.com>
2023-01-07 15:44:49 +0200

    compile file

---
+++ compile.bat
@@ -0,0 +1,19 @@
+pyuic5 forms/login_view.ui -o views/login_view.py
+pyuic5 forms/signup_view.ui -o views/signup_view.py
+pyuic5 forms/chose_your_goal_view.ui -o views/chose_your_goal_view.py
+pyuic5 forms/signup_doctor_view.ui -o views/signup_doctor_view.py
+pyuic5 forms/models.ui -o views/models.py
+pyuic5 forms/successfully.ui -o views/successfully.py
+pyuic5 forms/alzhimer_model.ui -o views/alzhimer_model.py
+pyuic5 forms/blood_model.ui -o views/blood_model.py
+pyuic5 forms/diabetes_model.ui -o views/diabetes_model.py
+pyuic5 forms/heart_model.ui -o views/heart_model.py
+pyuic5 forms/parkinson_molde.ui -o views/parkinson_molde.py
+pyuic5 forms/breast_model.ui -o views/breast_model.py
+pyuic5 forms/models_patient.ui -o views/models_patient.py
+
+
+
+
+pyrcc5 forms/app_resources.qrc -o app_resources_rc.py
+
