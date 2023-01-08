f6d8d49ce93654ece08001e651c5bae855a75862
yosef <yousefelmesalamy@gmail.com>
2023-01-09 00:27:45 +0200

    creat parkinson ui creat main screen add some icons

---
+++ compiler.bat
@@ -0,0 +1,9 @@
+pyuic5 forms/login_view.ui -o views/login_view.py
+pyuic5 forms/goal_view.ui -o views/goal_view.py
+pyuic5 forms/register_view.ui -o views/register_view.py
+pyuic5 forms/main_view.ui -o views/main_view.py
+pyuic5 forms/parkinson_molde.ui -o views/parkinson_molde.py
+
+
+
+pyrcc5 forms/app_resources.qrc -o app_resources_rc.py
